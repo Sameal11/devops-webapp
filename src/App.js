4686c67a3d3bb328bd9ae3201cc485bd19f85def
Sameal11 <aryanrajarya11@gmail.com>
2026-02-22 04:16:36 +0530

    Testing full CI/CD pipeline

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to DevOps WebApp</h1>
+        <h1>"CI/CD Automation Test Succesfull</h1>
         <h4>Created by: Aryan Raj Arya </h4>
+
       </header>
     </div>
   );

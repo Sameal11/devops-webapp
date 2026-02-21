3c92e02c5b9a30324640e784bdd0f2305e15ee88
Sameal11 <aryanrajarya11@gmail.com>
2026-02-22 02:42:46 +0530

    initial  web app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to DevOps WebApp</h1>
+        <h4>Created by: Aryan Raj Arya </h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
